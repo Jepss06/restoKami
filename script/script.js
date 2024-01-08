d6cd81399a2469ecf0883962dc8e67bc2d15f767
Jepss06 <blackelimit@gmail.com>
2024-01-08 07:42:15 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,7 @@
 const d = document;
-let restoPay = d.getElementById("resto-pay");
-let cash = d.getElementById("cash");
-let info = d.getElementById("info");
+let restoPay = d.getElementById("resto-pay").checked;
+let cash = d.getElementById("cash").checked;
 let saldo = d.getElementById("saldo-resto-pay").value; // saldo resto pay
-let totalPembayaran = d.getElementById("total-pembayaran").value; // total pembayaran
 console.log(localStorage.getItem("total"));
 const formatUang = (rupiah) => {
   return rupiah.toLocaleString("id-ID", {
@@ -24,68 +22,25 @@ let saldo_u = 50000;
 const saldo_user = (document.getElementById("saldo").innerHTML =
   formatUang(saldo_u));
 const bayar = () => {
-  let restoPay = d.getElementById("resto-pay").checked;
-  let cash = d.getElementById("cash").checked;
-
-  console.log("pulih restopay " + restoPay);
-  console.log("pilih cash " + cash);
-  console.log("saldo " + saldo);
-  console.log("totalPembayaran " + totalPembayaran);
+  console.log(restoPay, cash);
   // Cek apakah memilih metode resto-pay
   if (!restoPay && !cash) {
-    openModal();
-    d.getElementById("message").innerHTML =
-      "Plih metode pembayaran terlebih dahulu !";
+    alert("Plih metode pembayaran terlebih dahulu !");
   } else {
     if (restoPay === true) {
       if (saldo_u < data) {
-        openModal();
-        d.getElementById("message").innerHTML = "Saldo Tidak Cukup !";
-        d.getElementById("imgpayment").src = "./assets/img/image 2.png";
+        konfirmasi = confirm("Saldo Tidak Cukup ! apakah anda ingin topup?");
+        if (konfirmasi == true) {
+          topup = prompt("masukan nomina pecahan 10000");
+          if (topup % 10000 == 0) {
+            saldo_u + topup;
+          }
+        }
       } else {
         window.location.href = "succes.html";
       }
     } else if (cash === true) {
-      console.log("berhasil pindah halman ke cash");
       window.location.href = "cashier.html";
     }
   }
 };
-
-// open modal
-const openModal = () => {
-  d.getElementById("myModal").style.display = "block";
-  d.getElementById("overlay").style.display = "block";
-};
-
-// close modal
-const closeModal = () => {
-  d.getElementById("myModal").style.display = "none";
-  d.getElementById("overlay").style.display = "none";
-};
-
-d.getElementById("closeModal").addEventListener("click", () => {
-  closeModal();
-});
-
-d.getElementById("overlay").addEventListener("click", () => {
-  closeModal();
-});
-
-// klik container pembayaran
-
-const klikRestoPay = d.querySelector(".container-metode-pembayaran");
-const klikCash = d.querySelector(".container-metode-cash");
-
-// restoPay = d.getElementById("resto-pay") = true
-let checkedResto = false;
-klikRestoPay.addEventListener("click", () => {
-  checkedResto = !checkedResto;
-  restoPay.checked = checkedResto;
-});
-
-let checkedCash = false;
-klikCash.addEventListener("click", () => {
-  checkedCash = !checkedCash;
-  cash.checked = checkedCash;
-});
